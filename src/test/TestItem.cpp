71360564b0324f09e1c69dd61cfb18ff3e2505f8
prosbloom225 <prosbloom@gmail.com>
2017-11-11 23:20:34 +0000

    adding mod to item test

--- src/test/TestItem.cpp
+++ src/test/TestItem.cpp
@@ -3,8 +3,9 @@
 
 
 TEST_CASE("Test basic item", "[item]") {
-    Item *item = new Item("testItem1");
+    Item *item = new Item("test", "testItem1");
 
     // require all properties here
     REQUIRE(strcmp(item->getName(), "testItem1") == 0);
+    REQUIRE(strcmp(item->getModName(), "test") == 0);
 }
