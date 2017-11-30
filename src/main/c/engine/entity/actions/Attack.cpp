16102f75a3d84ecc290c8597117d691fd898ab7c
prosbloom225 <prosbloom@gmail.com>
2017-11-30 22:49:33 +0000

    logging

--- src/main/c/engine/entity/actions/Attack.cpp
+++ src/main/c/engine/entity/actions/Attack.cpp
@@ -5,6 +5,7 @@ Attack::Attack(BaseEntity *defender, double amount){
     this->defender = defender;
 }
 void Attack::execute(){
+    LOG(INFO) << "Attack: " << defender->getHp() << "-" << amount;
     defender->setHp(defender->getHp() - amount);
     delete this;
 }
